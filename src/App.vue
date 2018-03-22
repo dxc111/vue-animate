c81dff36fc9d6bd1d616e5bd87532fe06944a4a5
dxc111 <dx131477@outlook.com>
2018-03-22 08:56:47 +0800

    添加 parallax

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <router-link to="/">home</router-link>
+    <router-link to="/parallax">parallax</router-link>
     <router-view/>
   </div>
 </template>
---
+++ src/components/Parallax.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="scene">
+    <div data-depth="0.2">My first Layer!</div>
+    <div data-depth="0.6">My second Layer!</div>
+  </div>
+</template>
+
+<script>
+import Parallax from 'parallax-js'
+
+export default {
+  name: 'Parallax',
+  mounted () {
+    // eslint-disable-next-line
+    const parallaxInstance = new Parallax(document.getElementById('scene'))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
