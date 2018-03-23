8cb2a6ceb32bc9c3c1087bb10c29adc2bd8ee2f7
dxc111 <dx131477@outlook.com>
2018-03-23 13:23:27 +0800

    fix paper

--- src/components/Paper.vue
+++ src/components/Paper.vue
@@ -6,36 +6,66 @@
 
 <script>
 import {paper} from 'paper'
-import wow from 'wowjs/dist/wow'
+// import wow from 'wowjs/dist/wow'
 
 export default {
   name: 'Paper',
   mounted () {
-    console.info(wow)
     var canvas = document.getElementById('myCanvas')
     // Create an empty project and a view for the canvas:
     paper.setup(canvas)
-    // Create a Paper.js Path to draw a line into it:
-    var path = new paper.Path()
-    // Give the stroke a color
-    path.strokeColor = 'black'
-    var start = new paper.Point(100, 100)
-    // Move to start and draw a line from there
-    path.moveTo(start)
-    // Note that the plus operator on Point objects does not work
-    // in JavaScript. Instead, we need to call the add() function:
-    path.lineTo(start.add([ 200, -50 ]))
-    // Draw the view now:
+    // Adapted from the following Processing example:
+    // http://processing.org/learning/topics/follow3.html
+
+    // The amount of points in the path:
+    var points = 25
+
+    // The distance between the points:
+    var length = 35
+    var path = new paper.Path({
+      strokeColor: '#E4141B',
+      strokeWidth: 20,
+      strokeCap: 'round'
+    })
+    var start = new paper.Point(paper.view.center.x / 10, paper.view.center.y)
+    console.info(start)
+    for (var i = 0; i < points; i++) {
+      path.add(new paper.Point((start.x + i * length), start.y))
+    }
+
     paper.view.draw()
     // eslint-disable-next-line
-    paper.view.onFrame = function (event) {
-    // On each frame, rotate the path by 3 degrees:
-      path.rotate(3)
+    paper.view.onMouseMove = (event) => {
+      path.firstSegment.point = event.point
+      for (var i = 0; i < points - 1; i++) {
+        var segment = path.segments[i]
+        var nextSegment = segment.next
+        var vectorPoint = this.minusPoint(segment.point, nextSegment.point)
+        vectorPoint.length = length
+        nextSegment.point = this.minusPoint(segment.point, vectorPoint)
+      }
+      path.smooth({ type: 'continuous' })
+    }
+    paper.view.onMouseDown = (event) => {
+      path.fullySelected = true
+      path.strokeColor = '#e08285'
+    }
+    paper.view.onMouseUp = (event) => {
+      path.fullySelected = false
+      path.strokeColor = '#e4141b'
+    }
+  },
+  methods: {
+    minusPoint (prePonit, netxPoint) {
+      return new paper.Point(prePonit.x - netxPoint.x, prePonit.y - netxPoint.y)
     }
   }
 }
 </script>
 
 <style scoped>
-
+#myCanvas{
+  width: 100vw;
+  height: 80vh;
+}
 </style>
